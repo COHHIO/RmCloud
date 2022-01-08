e225abdb0f3a4697fb85e7b31386b3e08e9efe65
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2022-01-08 17:26:19 -0500

    implement `tryCatchLog` in `to_console`

--- R/mod_body_utils.R
+++ R/mod_body_utils.R
@@ -1,12 +1,17 @@
 choices <- purrr::map(rlang::fn_fmls(RmData::daily_update)[c("steps", "funs")], eval)
 
+futile.logger::flog.appender(futile.logger::appender.console())
 
-
-to_console <- function(x, mc = match.call(), id = "console") {
+to_console <- function(x, mc = match.call(), id = "console", session) {
   .mc <- paste0(rlang::expr_deparse(mc), collapse = " ")
   x <- rlang::enquo(x)
   debugonce(cli::cli_alert_success)
-  withCallingHandlers(rlang::eval_tidy(x),
+  tryCatchLog::tryCatchLog(rlang::eval_tidy(x),
+                           write.error.dump.file = FALSE,,
+                           silent.warnings = FALSE,
+                           silent.messages = FALSE,
+                           include.compact.call.stack = TRUE,,
+                           logged.conditions = NA,
                       warning = function (w) {
                         .call <- paste0(rlang::expr_deparse(w$call), collapse = " ")
                         .m <- paste0(w$message, collapse = "\n")
@@ -16,10 +21,16 @@ to_console <- function(x, mc = match.call(), id = "console") {
                       error = function (e) {
                         .m <- paste0(e$message, collapse = "\n")
                         shinyjs::html(id = id, html = as.character(tags$code(class = 'text-danger',"Error in ", .mc,":", tags$strong(.m), tags$br())), add = TRUE)
+                        shiny::showNotification("Reloading app. You can resume where from where the error occured.", type = "error")
+                        session$reload()
                       },
                       message = function (m) function(m) {
+                        browser()
                         .m <- paste0(m$message, collapse = "\n")
                         shinyjs::html(id = id, html = as.character(tags$p(class = 'text-info', .m, tags$br())), add = TRUE)
-                      }
+                      },
+           finally = {
+
+           }
   )
 }
