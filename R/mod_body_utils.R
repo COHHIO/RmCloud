9e42ebd07f1f63eedb3c6f718dc6e1b8d7fec92b
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2022-01-08 19:37:07 -0500

    info & above to shiny console **finally working**

--- R/mod_body_utils.R
+++ R/mod_body_utils.R
@@ -1,36 +1,42 @@
 choices <- purrr::map(rlang::fn_fmls(RmData::daily_update)[c("steps", "funs")], eval)
+info_to_shiny_console <- function ()
+{
+  function(line) {
+    .class <- switch(stringr::str_extract(line, "^\\w+"),
+                     TRACE = ,
+                     DEBUG = 'text-primary',
+                     INFO = 'text-info',
+                     WARN = 'text-warning',
+                     FATAL = ,
+                     ERROR = 'text-danger')
+    shinyjs::html(id = "console", html = as.character(tags$code(class = .class, tags$strong(line), tags$br())), add = TRUE)
+    cat(line, sep = "")
+  }
+}
 
-futile.logger::flog.appender(futile.logger::appender.console())
-
+futile.logger::flog.appender(info_to_shiny_console())
+futile.logger::flog.threshold(INFO)
 to_console <- function(x, mc = match.call(), id = "console", session) {
   .mc <- paste0(rlang::expr_deparse(mc), collapse = " ")
   x <- rlang::enquo(x)
-  debugonce(cli::cli_alert_success)
-  tryCatchLog::tryCatchLog(rlang::eval_tidy(x),
-                           write.error.dump.file = FALSE,,
-                           silent.warnings = FALSE,
-                           silent.messages = FALSE,
-                           include.compact.call.stack = TRUE,,
-                           logged.conditions = NA,
-                      warning = function (w) {
-                        .call <- paste0(rlang::expr_deparse(w$call), collapse = " ")
-                        .m <- paste0(w$message, collapse = "\n")
 
-                        shinyjs::html(id = id, html = as.character(tags$code(class = 'text-warning', "Warning in ", .call,":", tags$strong(.m), tags$br())), add = TRUE)
-                      },
+  tryCatchLog::tryCatchLog(rlang::eval_tidy(x),
+                           write.error.dump.file = FALSE,
+                           silent.warnings = FALSE,
+                           silent.messages = TRUE,
+                           include.compact.call.stack = FALSE,
+                           include.full.call.stack = FALSE,
+                           logged.conditions = NA,
                       error = function (e) {
                         .m <- paste0(e$message, collapse = "\n")
-                        shinyjs::html(id = id, html = as.character(tags$code(class = 'text-danger',"Error in ", .mc,":", tags$strong(.m), tags$br())), add = TRUE)
+                        .fn <- glue::glue("ERROR_{Sys.time()}.feather")
+                        UU::object_write(tryCatchLog::last.tryCatchLog.result(), .fn)
+                        shiny::showNotification(paste0("Error: ", .m, " logged to ", .fn))
                         shiny::showNotification("Reloading app. You can resume where from where the error occured.", type = "error")
                         session$reload()
                       },
-                      message = function (m) function(m) {
-                        browser()
-                        .m <- paste0(m$message, collapse = "\n")
-                        shinyjs::html(id = id, html = as.character(tags$p(class = 'text-info', .m, tags$br())), add = TRUE)
-                      },
-           finally = {
-
-           }
+                      finally = {
+                        #reserved
+                      }
   )
 }
