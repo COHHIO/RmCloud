ba94d256ef91ae9f757296b0b15d40db753cd8b6
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2022-01-15 06:27:14 -0500

    Working `auto-scroll` and `auto-close`

--- inst/app/www/js/auto-scroll.js
+++ inst/app/www/js/auto-scroll.js
@@ -1,6 +1,17 @@
-$('#console').ready(function(){
-  window.setInterval(function() {
-  var elem = document.getElementById('console');
-  elem.scrollTop = elem.scrollHeight;
-}, 5000);
+$('#body-console').ready(function(){
+
+var add = setInterval(function() {
+  var out = document.getElementById('body-console')
+    // allow 1px inaccuracy by adding 1
+    var isScrolledToBottom = out.scrollHeight - out.clientHeight >= out.scrollTop;
+    console.log(out.scrollHeight - out.clientHeight,  out.scrollTop);
+
+    // scroll to bottom if isScrolledToBotto
+    if(isScrolledToBottom)
+      out.scrollTop = out.scrollHeight - out.clientHeight;
+}, 1000);
+})
+
+$('#body-run').ready(function(){
+  $('#body-run').click(function() {$('#controls button[data-card-widget="collapse"]').trigger('click')})
 })
